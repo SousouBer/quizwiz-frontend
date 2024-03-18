4073e89851df6aadf932ceb1fa33ce9ff23d81ab
Soso <soso.beriashvili@redberry.ge>
2024-03-18 20:36:41 +0400

    style: complete styling landing page main section

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,8 +1,40 @@
 <template>
   <div>
     <the-header></the-header>
-    <main>
-      <h1>Landing works!</h1>
+    <main class="relative overflow-hidden">
+      <section class="px-24 pt-6 pb-32">
+        <div>
+          <img
+            src="../assets/images/quizwiz-title.svg"
+            alt="Quiz wiz Title Logo"
+          />
+        </div>
+        <p class="text-4xl font-semibold text-gray-500">
+          Play with us, Explore with us
+        </p>
+      </section>
+      <section class="px-24 bg-orange-500 flex flex-col py-14">
+        <span class="text-8xl font-black text-white">200+</span>
+        <span class="text-5xl font-black text-white">Quiz games</span>
+      </section>
+      <section class="px-24 bg-blue-500 flex flex-col py-10">
+        <span class="text-8xl font-black text-white">25+</span>
+        <span class="text-5xl font-black text-white">Different genre</span>
+      </section>
+      <div class="absolute bottom-0 right-0 transform translate-y-1/3">
+        <div class="relative">
+          <img
+            src="../assets/images/black-background.svg"
+            alt="Black background"
+          />
+          <div class="absolute top-0 right-0 landing-person-img">
+            <img
+              src="../assets/images/landing-person.svg"
+              alt="Landing Person"
+            />
+          </div>
+        </div>
+      </div>
     </main>
     <the-footer></the-footer>
   </div>
