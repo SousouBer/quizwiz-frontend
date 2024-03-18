2546d679b9c652037f10c46670b8e82108377907
Soso <soso.beriashvili@redberry.ge>
2024-03-18 14:52:46 +0400

    style: style header for landing page

--- src/components/shared/TheHeader.vue
+++ src/components/shared/TheHeader.vue
@@ -1,5 +1,23 @@
 <template>
-  <p>Header here!</p>
+  <header class="flex justify-between items-center px-20 py-4">
+    <div class="flex items-center gap-14">
+      <div>
+        <a href="#">
+          <img
+            src="/src/assets/images/quizwiz-header-logo.svg"
+            alt="Quiz wiz Logo"
+          />
+        </a>
+      </div>
+      <a href="#" class="text-475467"> Quizzes </a>
+    </div>
+    <div class="flex justify-center items-center gap-4">
+      <button class="text-white bg-black rounded px-6 py-3.5 text-sm font-bold">
+        Sign up
+      </button>
+      <button class="text-4B69FD">Log in</button>
+    </div>
+  </header>
 </template>
 
 <script>
