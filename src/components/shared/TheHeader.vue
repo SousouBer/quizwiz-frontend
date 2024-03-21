eb71818f88962047507c58ebd421238db877a4f5
Soso <soso.beriashvili@redberry.ge>
2024-03-21 21:08:47 +0400

    feat: add seach input field in the header

--- src/components/shared/TheHeader.vue
+++ src/components/shared/TheHeader.vue
@@ -18,22 +18,82 @@
       </div>
     </div>
     <div class="flex justify-center items-center gap-1">
-      <base-button class="text-white bg-black hover:bg-gray-900"
-        >Sign up</base-button
+      <div
+        :class="{ 'active-searchbar': isFirstFocused }"
+        class="w-28 border border-transparent h-11 overflow-hidden transition-all duration-300 ease-out"
       >
-      <base-button class="text-blue-500 hover:text-blue-600"
-        >Log in</base-button
-      >
+        <div
+          class="flex flex-row items-center justify-center h-full transition-all duration-300 ease-out"
+        >
+          <div class="flex items-center justify-center gap-1 px-4 w-full">
+            <div class="flex items-center justify-center">
+              <img
+                class="w-full h-full"
+                src="/src/assets/images/search-quizzes-icon.svg"
+                alt="Search icon"
+              />
+            </div>
+            <input
+              @focus="toggleElement"
+              v-model="searchInput"
+              class="bg-transparent placeholder:text-sm w-full placeholder:px-1 outline-none transition-all duration-300 ease-out"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+          <div
+            v-if="isFirstFocused"
+            @click="closeInputField"
+            class="close-input border-l h-full rounded-tr-10 rounded-br-10 flex items-center justify-center w-12 cursor-pointer"
+          >
+            <img
+              src="/src/assets/images/hide-input-x.svg"
+              alt="Hide input icon"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/src/assets/images/unauthenticated-user-icon.svg"
+          alt="Unauthenticated user icon"
+        />
+      </div>
     </div>
   </header>
 </template>
 
 <script>
-import BaseButton from "../ui/buttons/BaseButton.vue";
+// import BaseButton from "../ui/buttons/BaseButton.vue";
 
 export default {
   components: {
-    BaseButton,
+    // BaseButton,
+  },
+
+  data() {
+    return {
+      isFirstFocused: false,
+      searchInput: "",
+    };
+  },
+
+  methods: {
+    toggleElement() {
+      this.isFirstFocused = true;
+    },
+    closeInputField() {
+      this.isFirstFocused = false;
+      this.searchInput = "";
+    },
   },
 };
 </script>
+
+<style scoped>
+.active-searchbar {
+  border: 1px solid gray;
+  border-radius: 10px;
+  width: 24rem;
+}
+</style>
