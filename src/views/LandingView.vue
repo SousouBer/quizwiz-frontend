678f549af1c09f77e8582470420acd413cbd40a7
Soso <soso.beriashvili@redberry.ge>
2024-03-26 13:55:02 +0400

    refactor: remove unnecessary divs from landing view

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -2,16 +2,14 @@
   <div>
     <the-header></the-header>
     <main class="relative overflow-hidden">
-      <section
+      <div
         class="h-screen overflow-hidden md:h-auto py-6 px-16 md:px-24 md:pt-6 md:pb-32 relative"
       >
-        <div class="w-72 sm:w-1/2">
-          <img
-            class="w-full"
-            src="@/assets/images/quizwiz-title.svg"
-            alt="Quiz wiz Title Logo"
-          />
-        </div>
+        <img
+          class="w-72 sm:w-1/2"
+          src="@/assets/images/quizwiz-title.svg"
+          alt="Quiz wiz Title Logo"
+        />
         <h2 class="text-lg sm:text-4xl font-semibold text-gray-500">
           Play with us, Explore with us
         </h2>
@@ -21,39 +19,37 @@
             src="@/assets/images/black-background-mobile.svg"
             alt="Black background"
         /></layouts-landing>
-      </section>
-      <section
-        class="sm:px-24 bg-vibrant-orange flex flex-col gap-4 py-14 px-10"
-      >
+      </div>
+      <div class="sm:px-24 bg-vibrant-orange flex flex-col gap-4 py-14 px-10">
         <span class="text-7xl sm:text-8xl font-black text-white">200+</span>
-        <div>
-          <div class="flex flex-row gap-5 items-center">
-            <router-link
-              to="/landing"
-              class="underline sm:no-underline text-5xl font-black text-white"
-            >
-              Quiz games
-            </router-link>
-            <icon-show-more-quizzes class="sm:hidden"></icon-show-more-quizzes>
-          </div>
+        <div class="flex flex-row gap-5 items-center">
+          <router-link
+            to="/landing"
+            class="underline sm:no-underline text-5xl font-black text-white"
+          >
+            Quiz games
+          </router-link>
+          <icon-show-more-quizzes
+            class="w-20 sm:hidden"
+          ></icon-show-more-quizzes>
         </div>
-      </section>
-      <section
-        class="sm:px-24 bg-saturated-blue flex flex-row py-10 px-10 pr-24"
+      </div>
+      <div
+        class="sm:px-24 bg-saturated-blue flex flex-col gap-4 py-10 px-10 pr-24"
       >
-        <div class="flex flex-col gap-4">
-          <span class="text-7xl sm:text-8xl font-black text-white">25+</span>
+        <span class="text-7xl sm:text-8xl font-black text-white">25+</span>
+        <div class="flex flex-row gap-5 items-center">
           <router-link
             to="/landing"
             class="underline sm:no-underline text-5xl font-black text-white"
           >
             Different genre
           </router-link>
+          <icon-show-more-quizzes
+            class="sm:hidden w-20"
+          ></icon-show-more-quizzes>
         </div>
-        <div class="flex items-center justify-center">
-          <icon-show-more-quizzes class="sm:hidden"></icon-show-more-quizzes>
-        </div>
-      </section>
+      </div>
       <layouts-landing class="hidden sm:block">
         <img
           class="w-full"
