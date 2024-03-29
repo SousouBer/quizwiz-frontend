1a804bb2d4c066c74165b9afd7c4fec2e408c646
Soso <soso.beriashvili@redberry.ge>
2024-03-29 16:05:07 +0400

    refactor: separate checkbox styles to css file

--- src/components/ui/FilterPanelModalCheckbox.vue
+++ src/components/ui/FilterPanelModalCheckbox.vue
@@ -11,25 +11,6 @@
   </div>
 </template>
 
-<style scoped>
-input[type="checkbox"] {
-  appearance: none;
-  cursor: pointer;
-  height: 1rem;
-  width: 1rem;
-  border-radius: 4px;
-  border: 1px solid #d0d5dd;
-}
-
-input[type="checkbox"]:checked {
-  background-color: #000;
-}
-
-input[type="checkbox"]:checked + .show-tick-icon {
-  display: block;
-}
-</style>
-
 <script>
 import IconCheck from "@/components/icons/IconCheck.vue";
 
@@ -58,3 +39,7 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@import "/src/assets/filterPanelModalCheckboxStyles.css";
+</style>
