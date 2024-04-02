1a88d37033f6540ef906953ac8899e4fd4970ed1
Soso <soso.beriashvili@redberry.ge>
2024-04-02 22:00:06 +0400

    refactor: apply custom styles to radio btn in register view

--- src/components/ui/form/InputRadio.vue
+++ src/components/ui/form/InputRadio.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="flex flex-row gap-3 my-6">
-    <input class="w-5 h-5" type="radio" :id="identifier" :name="identifier" />
+  <div class="flex flex-row items-center gap-3 my-6 relative">
+    <input
+      class="w-5 h-5 input-radio"
+      type="radio"
+      :id="identifier"
+      :name="identifier"
+    />
+    <IconTick
+      width="15"
+      height="15"
+      class="cursor-pointer absolute hidden show-tick-icon top-0 left-0 transform translate-x-1/4 translate-y-1/3"
+      @click="uncheckCheckbox"
+    />
     <label :for="identifier">{{ label }}</label>
   </div>
 </template>
 
 <script>
+import IconTick from "@/components/icons/IconTick.vue";
+
 export default {
+  components: {
+    IconTick,
+  },
   props: {
     label: {
       type: String,
@@ -15,6 +31,18 @@ export default {
     identifier: {
       type: String,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+
+  methods: {
+    uncheckCheckbox() {
+      this.isChecked = false;
     },
   },
 };
