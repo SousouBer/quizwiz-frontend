17e027dac6664a94937546866e4b33754226cdeb
Soso <soso.beriashvili@redberry.ge>
2024-04-07 23:24:40 +0400

    chore: add quiz card icons and test image

---
+++ src/components/quiz/QuizCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>
+      <img src="" alt="Quiz card image" />
+      <IconDifficultyLevel />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconDifficultyLevel from "@/components/icons/IconDifficultyLevel.vue";
+export default {
+  components: {
+    IconDifficultyLevel,
+  },
+};
+</script>
--- src/views/QuizzesView.vue
+++ src/views/QuizzesView.vue
@@ -2,6 +2,9 @@
   <!-- <TheHeader /> -->
   <main>
     <FilterPanel />
+    <div>
+      <QuizCard />
+    </div>
   </main>
   <TheFooter />
 </template>
@@ -10,12 +13,14 @@
 // import TheHeader from "@/components/shared/TheHeader.vue";
 import TheFooter from "@/components/shared/TheFooter.vue";
 import FilterPanel from "@/components/filter/FilterPanel.vue";
+import QuizCard from "@/components/quiz/QuizCard.vue";
 
 export default {
   components: {
     // TheHeader,
     TheFooter,
     FilterPanel,
+    QuizCard,
   },
 };
 </script>
