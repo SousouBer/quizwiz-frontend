e7aa3d445b5461eadddbf889736ec071025ab342
Soso <soso.beriashvili@redberry.ge>
2024-04-07 21:39:43 +0400

    refactor: create a separate component for sort controls

--- src/components/filter/FilterPanelModalSortBy.vue
+++ src/components/filter/FilterPanelModalSortBy.vue
@@ -2,32 +2,23 @@
   <layouts-filter-rounded-border>
     <layouts-filter-and-sort-by
       filterType="Sort by"
-      classNames="flex flex-col items-start justify-start gap-6 mt-6"
+      classNames="flex flex-col items-start justify-start gap-4 mt-6"
     >
-      <div
-        class="flex items-center gap-4 w-full px-3 py-2 rounded-xl bg-gray-100"
-      >
+      <FilterPanelModalSortControl criteria="A-Z">
         <IconAscending />
-        <span class="cursor-pointer font-semibold">A-Z</span>
-        <IconCheck width="16" height="16" color="green" />
-      </div>
-      <div class="flex items-center justify-center gap-4">
+      </FilterPanelModalSortControl>
+      <FilterPanelModalSortControl criteria="Z-A">
         <IconDescending />
-        <span class="cursor-pointer font-semibold">Z-A</span>
-        <div class="border border-red-500"></div>
-      </div>
-      <div class="flex items-center justify-center gap-4">
+      </FilterPanelModalSortControl>
+      <FilterPanelModalSortControl criteria="Most popular">
         <IconMostPopular />
-        <span class="cursor-pointer font-semibold">Most popular</span>
-      </div>
-      <div class="flex items-center justify-center gap-4">
+      </FilterPanelModalSortControl>
+      <FilterPanelModalSortControl criteria="Newest">
         <IconNewest />
-        <span class="cursor-pointer font-semibold">Newest</span>
-      </div>
-      <div class="flex items-center justify-center gap-4">
+      </FilterPanelModalSortControl>
+      <FilterPanelModalSortControl criteria="Oldest">
         <IconOldest />
-        <span class="cursor-pointer font-semibold">Oldest</span>
-      </div>
+      </FilterPanelModalSortControl>
     </layouts-filter-and-sort-by>
   </layouts-filter-rounded-border>
 </template>
@@ -35,23 +26,23 @@
 <script>
 import LayoutsFilterRoundedBorder from "@/components/layouts/LayoutsFilterRoundedBorder.vue";
 import LayoutsFilterAndSortBy from "@/components/layouts/LayoutsFilterAndSortBy.vue";
+import FilterPanelModalSortControl from "@/components/filter/FilterPanelModalSortControl.vue";
 import IconAscending from "@/components/icons/IconAscending.vue";
 import IconDescending from "@/components/icons/IconDescending.vue";
 import IconMostPopular from "@/components/icons/IconMostPopular.vue";
 import IconNewest from "@/components/icons/IconNewest.vue";
 import IconOldest from "@/components/icons/IconOldest.vue";
-import IconCheck from "@/components/icons/IconCheck.vue";
 
 export default {
   components: {
     LayoutsFilterRoundedBorder,
     LayoutsFilterAndSortBy,
+    FilterPanelModalSortControl,
     IconAscending,
     IconDescending,
     IconMostPopular,
     IconNewest,
     IconOldest,
-    IconCheck,
   },
 };
 </script>
