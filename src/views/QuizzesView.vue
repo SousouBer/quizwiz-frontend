b7e22ce246c38e3a62bb95dce9236f2331a96578
Soso <soso.beriashvili@redberry.ge>
2024-04-08 21:28:10 +0400

    style: add and style load more button in quiz listing

--- src/views/QuizzesView.vue
+++ src/views/QuizzesView.vue
@@ -1,10 +1,11 @@
 <template>
   <!-- <TheHeader /> -->
-  <main>
+  <main class="relative pb-32 border-b">
     <FilterPanel />
-    <div class="px-6 sm:px-24 grid md:grid-cols-3 gap-8">
+    <div class="px-6 sm:px-24 grid md:grid-cols-3 gap-8 relative">
       <QuizCard />
     </div>
+    <QuizButtonLoadMore />
   </main>
   <TheFooter />
 </template>
@@ -14,6 +15,7 @@
 import TheFooter from "@/components/shared/TheFooter.vue";
 import FilterPanel from "@/components/filter/FilterPanel.vue";
 import QuizCard from "@/components/quiz/QuizCard.vue";
+import QuizButtonLoadMore from "@/components/quiz/QuizButtonLoadMore.vue";
 
 export default {
   components: {
@@ -21,6 +23,7 @@ export default {
     TheFooter,
     FilterPanel,
     QuizCard,
+    QuizButtonLoadMore,
   },
 };
 </script>
