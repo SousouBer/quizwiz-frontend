c0ea0569ab90289aa22981c440bff0efa26cc706
Soso <soso.beriashvili@redberry.ge>
2024-04-16 20:56:52 +0400

    style: add general styles for question and answer

---
+++ src/components/quiz/QuizQuestionAndAnswers.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="flex items-center gap-6 mb-4">
+      <div class="relative">
+        <span class="text-saturated-blue font-semibold">Question - 1</span>
+        <IconMiddleLine
+          color="gray"
+          class="absolute top-0 right-0 transform translate-y-1/3 translate-x-3"
+        />
+      </div>
+      <span class="text-pinkish-mangeta font-semibold">Points - 2</span>
+    </div>
+    <span class="font-bold text-lg"
+      >What is the study of medicines and their action called?</span
+    >
+    <div @click="toggleAnswer" class="cursor-pointer">
+      <div
+        :class="{ 'border-saturated-blue-40 bg-saturated-blue-10': isChecked }"
+        class="border border-red rounded-lg flex justify-between items-center p-4"
+      >
+        <span
+          :class="{ 'text-saturated-blue': isChecked }"
+          class="text-gray-800"
+          >Drug Manufacturing</span
+        >
+        <div class="relative">
+          <input
+            :class="{ 'border border-light-gray': !isChecked }"
+            type="checkbox"
+            name="option_1"
+            id="id"
+            class="answer-checkbox w-4 h-4"
+            v-model="isChecked"
+          />
+          <IconTick
+            class="hidden show-tick-icon absolute top-0 right-0 transform -translate-x-1 translate-y-1"
+            height="10"
+            width="8"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconMiddleLine from "@/components/icons/IconMiddleLine.vue";
+import IconTick from "@/components/icons/IconTick.vue";
+
+export default {
+  components: {
+    IconMiddleLine,
+    IconTick,
+  },
+
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+
+  methods: {
+    toggleAnswer() {
+      this.isChecked = !this.isChecked;
+    },
+  },
+};
+</script>
--- src/views/QuizQuestionsView.vue
+++ src/views/QuizQuestionsView.vue
@@ -4,16 +4,24 @@
       <IconX width="24" height="24" color="gray" class="ml-auto" />
     </header>
     <QuizQuestionsQuizDetails />
+    <div class="flex justify-between px-24 py-16">
+      <div>
+        <QuizQuestionAndAnswers />
+      </div>
+      <div>timer goes here</div>
+    </div>
   </div>
 </template>
 
 <script>
 import QuizQuestionsQuizDetails from "@/components/quiz/QuizQuestionsQuizDetails.vue";
+import QuizQuestionAndAnswers from "@/components/quiz/QuizQuestionAndAnswers.vue";
 import IconX from "@/components/icons/IconX.vue";
 
 export default {
   components: {
     QuizQuestionsQuizDetails,
+    QuizQuestionAndAnswers,
     IconX,
   },
 };
