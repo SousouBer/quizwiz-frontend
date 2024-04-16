dc1c6551cf0d8dd46ab5aed85ae26a4e02b87e73
Soso <soso.beriashvili@redberry.ge>
2024-04-16 21:01:39 +0400

    refactor: create separate component for answers

---
+++ src/components/quiz/QuizAnswer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    @click="toggleAnswer"
+    :class="{ 'border-saturated-blue-40 bg-saturated-blue-10': isChecked }"
+    class="cursor-pointer border rounded-lg flex justify-between items-center p-4"
+  >
+    <span :class="{ 'text-saturated-blue': isChecked }" class="text-gray-800"
+      >Drug Manufacturing</span
+    >
+    <div class="relative">
+      <input
+        :class="{ 'border border-light-gray': !isChecked }"
+        type="checkbox"
+        name="option_1"
+        id="id"
+        class="answer-checkbox w-4 h-4"
+        v-model="isChecked"
+      />
+      <IconTick
+        class="hidden show-tick-icon absolute top-0 right-0 transform -translate-x-1 translate-y-1"
+        height="10"
+        width="8"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import IconTick from "@/components/icons/IconTick.vue";
+
+export default {
+  components: {
+    IconTick,
+  },
+
+  data() {
+    return {
+      isChecked: false,
+    };
+  },
+
+  methods: {
+    toggleAnswer() {
+      this.isChecked = !this.isChecked;
+    },
+  },
+};
+</script>
--- src/components/quiz/QuizQuestionAndAnswers.vue
+++ src/components/quiz/QuizQuestionAndAnswers.vue
@@ -13,56 +13,20 @@
     <span class="font-bold text-lg"
       >What is the study of medicines and their action called?</span
     >
-    <div @click="toggleAnswer" class="cursor-pointer">
-      <div
-        :class="{ 'border-saturated-blue-40 bg-saturated-blue-10': isChecked }"
-        class="border border-red rounded-lg flex justify-between items-center p-4"
-      >
-        <span
-          :class="{ 'text-saturated-blue': isChecked }"
-          class="text-gray-800"
-          >Drug Manufacturing</span
-        >
-        <div class="relative">
-          <input
-            :class="{ 'border border-light-gray': !isChecked }"
-            type="checkbox"
-            name="option_1"
-            id="id"
-            class="answer-checkbox w-4 h-4"
-            v-model="isChecked"
-          />
-          <IconTick
-            class="hidden show-tick-icon absolute top-0 right-0 transform -translate-x-1 translate-y-1"
-            height="10"
-            width="8"
-          />
-        </div>
-      </div>
+    <div>
+      <QuizAnswer />
     </div>
   </div>
 </template>
 
 <script>
+import QuizAnswer from "@/components/quiz/QuizAnswer.vue";
 import IconMiddleLine from "@/components/icons/IconMiddleLine.vue";
-import IconTick from "@/components/icons/IconTick.vue";
 
 export default {
   components: {
+    QuizAnswer,
     IconMiddleLine,
-    IconTick,
-  },
-
-  data() {
-    return {
-      isChecked: false,
-    };
-  },
-
-  methods: {
-    toggleAnswer() {
-      this.isChecked = !this.isChecked;
-    },
   },
 };
 </script>
