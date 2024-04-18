562c9f828f100a4be4c31c2773b929382f173cf2
Soso <soso.beriashvili@redberry.ge>
2024-04-18 13:22:18 +0400

    refactor: create separate component for header title and icons

--- src/views/QuizQuestionsView.vue
+++ src/views/QuizQuestionsView.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
     <header class="fixed z-10 bg-white border-b-2 py-6 h-20 w-full px-24 flex">
-      <div
-        class="flex gap-6 items-center rounded-lg border border-light-gray bg-gray-100 pb-2 pt-4 px-6"
-      >
-        <span class="font-bold text-base whitespace-nowrap pb-2 pr-4"
-          >Timeline of Discoveries</span
-        >
-        <span class="pb-2 pr-2">
-          <IconPin color="black" />
-        </span>
-        <QuizInnerStatistic statistic="questions" color="black" />
-        <QuizInnerStatistic statistic="points" color="black" />
-        <QuizInnerStatistic statistic="count" color="black" />
-        <QuizInnerStatistic statistic="time" color="black" />
-      </div>
+      <QuizQuestionsHeaderTitleAndIcons title="TITLE" />
       <IconX width="24" height="24" color="gray" class="ml-auto" />
     </header>
     <QuizQuestionsQuizDetails />
@@ -43,19 +30,17 @@
 <script>
 import QuizQuestionsQuizDetails from "@/components/quiz/QuizQuestionsQuizDetails.vue";
 import QuizQuestionAndAnswers from "@/components/quiz/QuizQuestionAndAnswers.vue";
+import QuizQuestionsHeaderTitleAndIcons from "@/components/quiz/QuizQuestionsHeaderTitleAndIcons.vue";
 import TheTimer from "@/components/quiz/TheTimer.vue";
 import IconX from "@/components/icons/IconX.vue";
-import IconPin from "@/components/icons/IconPin.vue";
-import QuizInnerStatistic from "@/components/quiz/QuizInnerStatistic.vue";
 
 export default {
   components: {
     QuizQuestionsQuizDetails,
     QuizQuestionAndAnswers,
     TheTimer,
+    QuizQuestionsHeaderTitleAndIcons,
     IconX,
-    IconPin,
-    QuizInnerStatistic,
   },
 };
 </script>
