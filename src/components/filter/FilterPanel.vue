dd04f35bd3b3cec9ece163cc1ec7da30b7afcfb6
Soso <soso.beriashvili@redberry.ge>
2024-04-25 03:37:17 +0400

    feat: add categories slider in filter panel

--- src/components/filter/FilterPanel.vue
+++ src/components/filter/FilterPanel.vue
@@ -2,7 +2,17 @@
   <div
     class="flex items-start sm:items-center flex-col sm:flex-row gap-3 sm:px-24 sm:py-12"
   >
-    <div class="flex items-center gap-4 pt-3 w-11/12 overflow-hidden border-b">
+    <div
+      @click="slide('left')"
+      class="hidden sm:inline-block transition-all duration-300 ease-in-out hover:bg-gray-200 p-2 rounded-full cursor-pointer"
+    >
+      <IconCategorySlider class="transform scale-x-[-1]" />
+    </div>
+
+    <div
+      ref="categoriesWrapper"
+      class="flex items-center gap-4 pt-3 w-11/12 overflow-hidden border-b scroll-smooth"
+    >
       <FilterPanelCategoryItem label="All Quizzes" id="all_quizzes" />
       <FilterPanelCategoryItem
         v-for="category in categories"
@@ -12,6 +22,7 @@
       />
     </div>
     <div
+      @click="slide('right')"
       class="hidden sm:inline-block transition-all duration-300 ease-in-out hover:bg-gray-200 p-2 rounded-full cursor-pointer"
     >
       <IconCategorySlider />
@@ -47,6 +58,11 @@ export default {
   methods: {
     toggleFilterModal() {
       this.showFilterModal = !this.showFilterModal;
+    },
+
+    slide(direction) {
+      const categoriesWrapper = this.$refs.categoriesWrapper;
+      categoriesWrapper.scrollLeft += direction === "left" ? -100 : 100;
     },
   },
 
