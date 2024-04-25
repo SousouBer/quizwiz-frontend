a3157852b69bcc17910b0f39a27453642f3ff75e
Soso <soso.beriashvili@redberry.ge>
2024-04-25 21:44:23 +0400

    refactor: separate main layout for error views

---
+++ src/components/layouts/LayoutsErrorPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col items-center gap-4 sm:gap-8 p-12">
+    <span class="font- Raleway font-black text-4xl sm:text-6xl">{{
+      label
+    }}</span>
+    <span class="font-bold text-lg">{{ description }}</span>
+    <img :src="src" :alt="description" />
+    <router-link
+      to="quizzes"
+      class="flex items-center justify-center gap-2 cursor-pointer"
+    >
+      <IconGoHome />
+      <span class="text-lg text-saturated-blue">Go home</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import IconGoHome from "@/components/icons/IconGoHome.vue";
+
+export default {
+  components: {
+    IconGoHome,
+  },
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,30 +1,23 @@
 <template>
   <TheHeader />
-  <div class="flex flex-col items-center gap-4 sm:gap-8 p-12">
-    <span class="font- Raleway font-black text-4xl sm:text-6xl">Oops!</span>
-    <span class="font-bold text-lg">Error 404</span>
-    <img src="@/assets/images/not-found-error.svg" alt="404 page" />
-    <router-link
-      to="quizzes"
-      class="flex items-center justify-center gap-2 cursor-pointer"
-    >
-      <IconGoHome />
-      <span class="text-lg text-saturated-blue">Go home</span>
-    </router-link>
-  </div>
+  <LayoutsErrorPage
+    label="Oops!"
+    description="Error 404"
+    src="/images/not-found-error.svg"
+  />
   <TheFooter />
 </template>
 
 <script>
 import TheHeader from "@/components/shared/TheHeader.vue";
 import TheFooter from "@/components/shared/TheFooter.vue";
-import IconGoHome from "@/components/icons/IconGoHome.vue";
+import LayoutsErrorPage from "@/components/layouts/LayoutsErrorPage.vue";
 
 export default {
   components: {
     TheHeader,
     TheFooter,
-    IconGoHome,
+    LayoutsErrorPage,
   },
 };
 </script>
