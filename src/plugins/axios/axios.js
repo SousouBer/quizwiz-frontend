5cab2e60f0fe8c916f71bfb894b0ad0dae3e9f41
Soso <soso.beriashvili@redberry.ge>
2024-04-27 02:06:49 +0400

    feat: add interceptor to log user out if 401 code is received

--- src/plugins/axios/axios.js
+++ src/plugins/axios/axios.js
@@ -1,9 +1,26 @@
 import axios from "axios";
+import store from "@/store/quiz/index";
+import router from "@/router";
 
 const instance = axios.create({
   baseURL: "http://127.0.0.1:8000",
   withCredentials: true,
   withXSRFToken: true,
 });
+
+instance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("userIsAuthenticated");
+
+      store.commit("setUserIsAuthenticated", false);
+      router.push({ name: "login" });
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
