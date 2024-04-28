207b1df4b0e961c538bcc75e393c6812d3d9a113
Soso <soso.beriashvili@redberry.ge>
2024-04-29 01:13:22 +0400

    fix: fix header quizzes link route

--- src/components/ui/links/LinkQuizzes.vue
+++ src/components/ui/links/LinkQuizzes.vue
@@ -1,11 +1,9 @@
 <template>
-  <div>
-    <router-link
-      to="/landing"
-      class="text-dark-blue-gray text-lg font-semibold hover:text-gray-600 transition-colors duration-300 ease-in-out"
-      >Quizzes</router-link
-    >
-  </div>
+  <router-link
+    to="quizzes"
+    class="text-dark-blue-gray text-lg font-semibold hover:text-gray-600 transition-colors duration-300 ease-in-out"
+    >Quizzes</router-link
+  >
 </template>
 
 <script>
