3373d825d6a193f8bf443d3c4e3aebde05c343dd
Soso <soso.beriashvili@redberry.ge>
2024-05-15 14:19:23 +0400

    style: add blue dot next to quizzes in header

--- src/components/ui/links/LinkQuizzes.vue
+++ src/components/ui/links/LinkQuizzes.vue
@@ -1,11 +1,31 @@
 <template>
-  <router-link
-    :to="{ name: 'quizzes' }"
-    class="text-dark-blue-gray text-lg font-semibold hover:text-gray-600 transition-colors duration-300 ease-in-out"
-    >Quizzes</router-link
-  >
+  <div class="relative">
+    <IconDotBlue
+      v-if="showBlueDot"
+      width="12"
+      height="12"
+      class="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-5"
+    />
+    <router-link
+      :to="{ name: 'quizzes' }"
+      class="text-dark-blue-gray text-lg font-semibold hover:text-gray-600 transition-colors duration-300 ease-in-out"
+      >Quizzes</router-link
+    >
+  </div>
 </template>
 
 <script>
-export default {};
+import IconDotBlue from "@/components/icons/IconDotBlue.vue";
+
+export default {
+  components: {
+    IconDotBlue,
+  },
+
+  computed: {
+    showBlueDot() {
+      return this.$route.name === "quizzes";
+    },
+  },
+};
 </script>
